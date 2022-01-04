451dbeb9ac48c7480d25c17feb2621546432bef0
yudate <y0303noki@gmail.com>
2022-01-04 16:43:18 +0900

    アイコン作成

--- lib/main.dart
+++ lib/main.dart
@@ -2,33 +2,11 @@ import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:yell_app/screen/login_check_page.dart';
 import 'package:firebase_core/firebase_core.dart';
-import 'package:timezone/data/latest.dart' as tz;
-import 'package:timezone/timezone.dart' as tz;
-import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 
-final FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin =
-    FlutterLocalNotificationsPlugin();
 Future<void> main() async {
-  _setupTimeZone();
   WidgetsFlutterBinding.ensureInitialized();
-
-  //iOS設定
-  final IOSInitializationSettings initializationSettingsIOS =
-      IOSInitializationSettings(
-    requestSoundPermission: true,
-    requestBadgePermission: true,
-    requestAlertPermission: true,
-  );
-
-  //initializationSettingsのオブジェクト作成
-  final InitializationSettings initializationSettings = InitializationSettings(
-    iOS: initializationSettingsIOS,
-    android: null,
-  );
-
-  await flutterLocalNotificationsPlugin.initialize(
-    initializationSettings,
-  );
+  const flavor = String.fromEnvironment('FLAVOR');
+  print('環境:$flavor');
 
   await Firebase.initializeApp();
   runApp(
@@ -39,11 +17,4 @@ Future<void> main() async {
       ),
     ),
   );
-}
-
-// タイムゾーンを設定する
-Future<void> _setupTimeZone() async {
-  tz.initializeTimeZones();
-  var tokyo = tz.getLocation('Asia/Tokyo');
-  tz.setLocalLocation(tokyo);
 }
