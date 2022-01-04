89e59f8fe2185da4905e5f7b697238f7c433fd91
yudate <y0303noki@gmail.com>
2022-01-04 21:33:15 +0900

    エラーコード整理

---
+++ lib/const.dart
@@ -0,0 +1,18 @@
+class Const {
+  /// エラーコード一覧
+  /// 色々なエラーコードを定数
+  ///
+
+  // 予期しないエラー
+  static const String UNEXPECTED_CODE = 'unexpected code';
+
+  /// 招待コード
+  // 招待コード不正
+  static const String INVALID_CODE = 'invalid code';
+
+  // 有効期限切れ
+  static const String EXPIRED_CODE = 'expired code';
+
+  // 自分自身のコード
+  static const String MYSELF_CODE = 'myself code';
+}
